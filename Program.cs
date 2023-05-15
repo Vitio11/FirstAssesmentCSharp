da1f668a1b43c9cce1181dd896d718c1802ea822
Vitio11 <129160425+Vitio11@users.noreply.github.com>
2023-05-15 10:10:25 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,20 +5,23 @@ string? tipo = ReadLine();
 
 Fruta? fruta = null;
 
-Write($"Ingrese la variedad de la {tipo}: ");
+
 switch (tipo!.ToLower())
 {
     case "manzana":
+    Write($"Ingrese la variedad de la {tipo}: ");
         string? variedad = ReadLine();
         fruta = new Manzana("Manzana", variedad!);
         break;
 
     case "banana":
+        Write($"Ingrese la madurez de la {tipo}: ");
         string? madurez = ReadLine();
         fruta = new Banana("Banana", madurez!);
         break;
 
     case "naranja":
+        Write($"Ingrese el dulzor de la {tipo}: ");
         string? dulzor = ReadLine();
         fruta = new Naranja("Naranja", dulzor!);
         break;
